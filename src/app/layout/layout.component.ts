889c9748910e3a86afefc2f0f9adfa051dc40571
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-04-06 12:05:14 -0400

    Instalado paquete disable-devtool, creado layout funcional

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import DisableDevtool from 'disable-devtool';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -10,6 +10,17 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /* http://localhost:port?par=jinkies */
+    
+    DisableDevtool({
+      md5: "65e60d4afa599855c4133c06e50c62b9",
+      url: "http://www.pjud.cl",
+      tkName: "par",
+      clearLog: true,
+      disableCut: true,
+      disableCopy: true,
+      disablePaste: true,
+    });
   }
 
 }
